662be1d5db3783ee15aaeee5f67f365c7bd1808b
Adham Mohamed <135978982+AdhamMohamed27@users.noreply.github.com>
2025-03-22 21:59:28 +0200

    Create get_OS.rs

---
+++ get_OS.rs
@@ -0,0 +1,22 @@
+use std::env;
+
+fn get_os() {
+    println!("Your OS is: {}", env::consts::OS);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: cargo run -- <command>");
+        std::process::exit(1);
+    }
+
+    match args[1].as_str() {
+        "get_os" => get_os(),
+        _ => {
+            eprintln!("Unknown command: {}", args[1]);
+            std::process::exit(1);
+        }
+    }
+}
